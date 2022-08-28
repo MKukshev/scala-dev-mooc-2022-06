c70baeb9ac5655a783c7bea0b0d1c3d6fe730ecc
Maxim Kukshev <m.kukshev@gmail.com>
2022-08-28 15:44:05 +0300

    hw #task_futures_sequence

--- src/main/scala/futures/task_futures_sequence.scala
+++ src/main/scala/futures/task_futures_sequence.scala
@@ -2,6 +2,7 @@ package futures
 
 import HomeworksUtils.TaskSyntax
 
+import scala.{:+, ::}
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success, Try}
 
@@ -25,38 +26,14 @@ object task_futures_sequence {
   def fullSequence[A](futures: List[Future[A]])
                      (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
 
-    val full: Future[List[Try[A]]] = Future.sequence(futures.map(_.transform(Success(_))))
-    val success: Future[List[A]] = ???
-    val throwable: Future[List[Throwable]] = ???
-    success zip throwable
-/*
-    val success: Future[List[A]] = Future.sequence {
-      full.map(_.collect {
-        case Success(a) => a
-      })
+   val empty = Future.successful((List.empty[A], List.empty[Throwable]))
+
+   futures.foldLeft(empty) {  (accum, ft) =>
+        for {
+          (success,fail)  <- accum
+          res <- ft.map( v => (success :+ v, fail)).recover(e => (success, (fail :+ e)))
+        } yield res
     }
-
-    val throwable: Future[List[Throwable]] = Future.sequence(full.map(_.collect {
-      case Failure(a) => a
-    }))
-
-    success zip throwable
-
- */
-/*
-    val full: Future[List[A]] = Future.sequence(futures.map(a => {
-      val p = Promise[A]
-      a.onComplete {
-        case Failure(exception) => p.failure(exception)
-        case Success(value) => p.success(value)
-      }
-      p.future
-    }))
-    val successes: Future[List[A]] = full.map(_.collect { case Success(x) => x })
-    val failures: Future[List[Throwable]] = full.map(_.collect { case Failure(x) => x })
-    successes zip failures
-*/
-
   }
 
 
